bd0a961e2e5ad8c2caa6406a94e6c39744813f1e
Edson <edsonsegurapesantes@gmail.com>
2026-09-07 17:02:45 -0500

    corregir URL de Render en configuración

---
+++ Frontend-OEA/config.js
@@ -0,0 +1,6 @@
+// config.js
+const CONFIG = {
+    // Si estás probando en tu PC, usa localhost. Si ya está en producción, usa tu enlace de Render.
+    // O puedes dejar directamente la URL de Render para que consuma siempre la nube:
+    API_URL: 'https://oeaguayas.onrender.com'
+};
